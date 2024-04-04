f8fd33781773c71eef25a4df9324ac793f3da2a6
Josephine Kim <josiek@bu.edu>
2024-04-04 07:45:10 -0400

    data.qmd

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,13 +1,11 @@
 library(tidyverse)
 
-# Load data from CSV
-loan_data <- read_csv(here::here("scripts", "index/index.csv"))
+load_data <- read_csv(here::here("scripts", "index/index.csv"))
 
-# Clean the data (replace this with your actual cleaning code)
-loan_data_clean <- loan_data  
+load_data_clean <- load_data  
 
-# Save cleaned data as .rds file
-saveRDS(loan_data_clean, file = here::here("scripts", "cleaned_index_dataset.rds"))
+saveRDS(load_data_clean, file = here::here("scripts", "cleaned_index_dataset.rds"))
 
 readRDS(file = here::here("scripts", "cleaned_index_dataset.rds"))
+
 
