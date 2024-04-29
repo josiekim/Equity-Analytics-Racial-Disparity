829a2b1c2442ede0af546d67112aefd641258473
Josephine Kim <josiek@bu.edu>
2024-04-29 14:24:40 -0400

    ahhh 2.0

--- scripts/load_and_clean_data.R
+++ scripts/load_and_clean_data.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
+save(cleaned_data, file = here::here("scripts/cleaned_data.RData"))
+
 load("scripts/cleaned_data.RData")
 
 saveRDS(cleaned_data, file = "scripts/cleaned_data.rds")
